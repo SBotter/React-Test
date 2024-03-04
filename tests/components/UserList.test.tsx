704e859fc65d5cb1fb9c47c80086e0a6e7a4ab1a
Sergio Botter <sergio.botter@gmail.com>
2024-03-03 16:47:23 -0800

    Add UserList component test

---
+++ tests/components/UserList.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import UserList from "../../src/components/UserList";
+import { User } from "../../src/entities";
+
+describe("UserList", () => {
+  it("should render no users when the users array is empty", () => {
+    render(<UserList users={[]} />);
+
+    expect(screen.getByText(/No users/i)).toBeInTheDocument();
+  });
+
+  it("should render a list of users", () => {
+    const users: User[] = [
+      { id: 1, name: "Sergio" },
+      { id: 2, name: "Botter" },
+    ];
+
+    render(<UserList users={users} />);
+
+    users.forEach((user) => {
+      const link = screen.getByRole("link", {
+        name: user.name,
+      });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute("href", `/users/${user.id}`);
+    });
+  });
+});

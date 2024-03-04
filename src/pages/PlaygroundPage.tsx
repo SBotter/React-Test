a61b0fe58f766bcdbb8b7dc4900f521b60ddb0ee
Sergio Botter <sergio.botter@gmail.com>
2024-03-03 18:03:11 -0800

    add ExpandableText component test

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,8 +1,11 @@
+import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
 import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return (
+    <ExpandableText text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique ducimus tempore aliquid asperiores assumenda alias corporis dolores voluptatibus quisquam voluptate sed exercitationem nemo placeat maiores earum quas aperiam illum ullam numquam, itaque dolorum quia est libero. Inventore omnis nam unde nesciunt libero pariatur? Sapiente, asperiores ipsam quis id sit illum veniam natus obcaecati libero eum in quia laborum voluptatem pariatur totam doloribus sunt iure laboriosam, dolore consectetur expedita fugit et voluptates. Alias, mollitia praesentium commodi porro quae doloremque officiis quidem perferendis! Incidunt est nulla illo! Quisquam illum quam itaque ad illo, tempore ullam neque, cum aut eum eos ipsam maiores!" />
+  );
 };
 
 export default PlaygroundPage;
